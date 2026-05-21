bb4a94c30179ea4b8387a325eeb4142f88d74f8e
Sunny0025-IT <Sunny0025-IT@users.noreply.github.com>
2026-05-21 13:42:46 +0700

    Initial commit

---
+++ .github/pull_request_template.md
@@ -0,0 +1,10 @@
+### Checklist
+
+- [ ] I added an entry to `CHANGELOG.md` if knowledge of this change could be valuable to users.
+- [ ] I linked any relevant issues from https://github.com/mapvina/mapvina-navigation-ios/issues
+
+### Description
+
+
+### Infos for Reviewer
+
---
+++ MapboxNavigation/MLNVectorTileSource.swift
@@ -0,0 +1,11 @@
+import Foundation
+import MapVina
+
+extension MLNVectorTileSource {
+    var isMapboxStreets: Bool {
+        guard let configurationURL else {
+            return false
+        }
+        return configurationURL.scheme == "mapbox" && configurationURL.host!.components(separatedBy: ",").contains("mapbox.mapbox-streets-v7")
+    }
+}
